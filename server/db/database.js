7b51a67faefbde784b55c435baaf6c04d5d9dfe8
Mehron Latifi <88285109+MehronS@users.noreply.github.com>
2021-11-05 23:48:13 -0400

    added SSL to new sequelize db creation

--- server/db/database.js
+++ server/db/database.js
@@ -3,8 +3,10 @@ const Sequelize = require("sequelize");
 const db = new Sequelize(
   process.env.DATABASE_URL || "postgres://localhost:5432/travel_guide",
   {
-    logging: false, // unless you like the logs
-    // ...and there are many other options you may want to play with
+    logging: false,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   }
 );
 
