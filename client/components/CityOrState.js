6d63effd954612c6671ab6d9ce07692e00d88db5
Mehron Latifi <88285109+MehronS@users.noreply.github.com>
2021-11-07 18:30:00 -0500

    changed up ranking system for cities

--- client/components/CityOrState.js
+++ client/components/CityOrState.js
@@ -16,7 +16,12 @@ function CityOrState(props) {
         </a>
       </h2>
       <fieldset className="to_consider">
-        <h3>Average Rating: {city.average_rating}</h3>
+        <h3>
+          Average Rating:{" "}
+          {Number(city.average_rating.toFixed(2))
+            ? city.average_rating.toFixed(2)
+            : "N/A"}
+        </h3>
         <div className="safety_div">
           <h3>Safety: {city.safety[props.countryName].text}</h3>
           <h4>{city.safety[props.countryName].subText}</h4>
